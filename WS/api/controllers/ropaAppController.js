3354cf16af0dfbff07c30dbfec8f7f6b09ac4e8c
Edgar Cuesta <edgarcufe@gmail.com>
2019-06-26 10:09:39 -0500

    Pequeños cambios

--- WS/api/controllers/ropaAppController.js
+++ WS/api/controllers/ropaAppController.js
@@ -13,16 +13,18 @@ exports.list_all_users= function(req, res){
 
 exports.create_a_user = function(req, res){
     var new_User = new User(req.body);
-    var new_Buyer = new Buyer(req.body);
-    new_User.save(function(err, user){
+    new_User.save(function(err,user){
 	if(err)
 	    res.send(err);
-	res.write(JSON.stringify(user));
-    });
-    new_Buyer.user = new_User._id;
-    new_Buyer.save(function(err, buyer){
-	if(err)
-	    res.send(err);
-	res.write(JSON.stringify(buyer));
+	else
+	{
+	    var new_Buyer = new Buyer(req.body);
+	    new_Buyer.user = user._id;		  
+	    new_Buyer.save(function(err, buyer){
+		if(err)
+		    res.send(err);
+		res.json(buyer);
+	    });
+	}
     });
 };
